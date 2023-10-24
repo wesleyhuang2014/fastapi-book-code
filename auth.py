c82552338cf3b950c0bf3707a9a9ed03f402d2e5
huangxp <huangxp@hotmail.com>
2023-10-24 09:33:44 +0000

    Global Authentication: Shared Secret

---
+++ auth.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+app = FastAPI()
+basic = HTTPBasic()
+secret_user: str = "newphone"
+secret_password: str = "whodis?"
+
+@app.get("/who")
+def get_user(creds: HTTPBasicCredentials = Depends(basic)) -> dict:
+    if(creds.username == secret_user and
+       creds.password == secret_password):
+        return {"username": creds.username, "password": creds.password}
+    raise HTTPException(status_code=401, detail="Hey!")
+
+if __name__ == "__main__":
+    uvicorn.run("auth:app", reload=True)
