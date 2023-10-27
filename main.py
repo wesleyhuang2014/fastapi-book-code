9a265590d1668eac8922b82b0e9e3862214f4c60
huangxp <huangxp@hotmail.com>
2023-10-27 13:58:33 +0000

    OAuth2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from web import explorer, creature
+from web import explorer, creature, user
 
 app = FastAPI()
 app.include_router(explorer.router)
 app.include_router(creature.router)
+app.include_router(user.router)
 
 @app.get("/")
 def top():
